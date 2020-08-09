954b76283223ed92dfb3d33cbed00f9bb35f9f67
Kerem Çelebi <kcelebi94@gmail.com>
2020-08-10 02:24:54 +0300

    Header Logo source updated.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 import "./Header.css";
 
+import headerLogo from "../../images/hepsiburada-logo.png";
+
 export default function Header() {
   return (
     <div className="row border-bottom" data-test="component-header">
       <div className="col-6">
-        <img
-          src="https://www.musteritemsilcisinebaglanma.com/wp-content/uploads/2019/09/hepsiburada-logo.png"
-          className="img-fluid"
-          alt="logo"
-        />
+        <img src={headerLogo} className="img-fluid" alt="logo" />
       </div>
       <div className="col-6 d-flex justify-content-center align-items-center">
         <span className="header-right-text">
